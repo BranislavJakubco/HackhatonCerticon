64948c0cf5a7cb0f5d4ed152ec6f0f1bc0488ec7
marmot753 <mart.motycka@atlas.cz>
2022-11-04 20:26:58 +0100

    .

--- inc/formular.inc.php
+++ inc/formular.inc.php
@@ -6,14 +6,23 @@
                         <input name="lokalita" type="text" class="kontakt-input" placeholder="Lokalita" /> <br>
                         <input name="school" type="text" class="kontakt-input" placeholder="Škola" /> <br>
                         <input name="yearEND" type="text" class="kontakt-input" placeholder="Rok ukončení" /> <br>
-                        <input name="java" type="checkbox" class="kontakt-input" />Java <br>
-                        <input name="python" type="checkbox" class="kontakt-input" />Python <br>
-                        <input name="cPlus" type="checkbox" class="kontakt-input" />C++ <br>
-                        <input name="cSharp" type="checkbox" class="kontakt-input" />C# <br>
+
+                        
+                        <div class="kontakt-input">
+                                <p>Jaké programovací jazyky ovládáš?</p>
+                                <input name="java" type="checkbox"/> Java<br>
+                                <input name="python" type="checkbox"/> Python<br> 
+                                <input name="cPlus" type="checkbox"/> C++<br> 
+                                <input name="cSharp" type="checkbox"/> C#<br> 
+                        </div>
+                        
+
                         <input name="jazyk" type="text" class="kontakt-input" placeholder="Jiné programovací jazyky" /> <br>
                         <input name="prefTechnology" type="text" class="kontakt-input" placeholder="Preferované technologie" /> <br>
-                        <input name="GDPR" type="checkbox" class="kontakt-input">Souhlasím se spracováním GDPR<br>
 
+                        <div class="kontakt-input">
+                                <input name="java" type="checkbox"/> Souhlasím se zpracováním GDPR<br>
+                        </div>
 
 
                         <input type="submit" value="Odeslat" />
