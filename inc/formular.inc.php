cea757bb9315b80785ab829e84d71cf984f8e11b
AsakaUchiha <kayoousek@gmail.com>
2022-11-04 22:12:25 +0100

    .

--- inc/formular.inc.php
+++ inc/formular.inc.php
@@ -1,19 +1,19 @@
-<div class="row">
+<div class="row container" style="margin-left: 10%">
         <div class="col-sm-12">
                 <form class="formstyle" action="./PHP/insert.inc.php" method="post">
-                        <h1 style="margin-bottom: 6%; text-align: center; color: white">Kontaktujte nás!</h1>
-                        <input name="name" type="text" class="kontakt-input" placeholder="Jméno" />
+                        <h1 style="margin-bottom: 6%; text-align: center;" class="textik">Kontaktujte nás!</h1>
+                        <input name="name" type="text" class="kontakt-input" placeholder="Jméno"/>
                         <input name="lokalita" type="text" class="kontakt-input" placeholder="Lokalita" /> <br>
                         <input name="school" type="text" class="kontakt-input" placeholder="Škola" /> <br>
                         <input name="yearEND" type="text" class="kontakt-input" placeholder="Rok ukončení" /> <br>
 
                         
                         <div class="kontakt-input">
-                                <p>Jaké programovací jazyky ovládáš?</p>
-                                <input name="java" type="checkbox"/> Java<br>
-                                <input name="python" type="checkbox"/> Python<br> 
-                                <input name="cPlus" type="checkbox"/> C++<br> 
-                                <input name="cSharp" type="checkbox"/> C#<br> 
+                                <p class="textik">Jaké programovací jazyky ovládáš?</p>
+                                <input name="java" type="checkbox" class="textik" /> Java<br>
+                                <input name="python" type="checkbox" class="textik"/> Python<br> 
+                                <input name="cPlus" type="checkbox" class="textik"/> C++<br> 
+                                <input name="cSharp" type="checkbox" class="textik"/> C#<br> 
                         </div>
                         
 
@@ -21,7 +21,7 @@
                         <input name="prefTechnology" type="text" class="kontakt-input" placeholder="Preferované technologie" /> <br>
 
                         <div class="kontakt-input">
-                                <input name="java" type="checkbox"/> Souhlasím se zpracováním GDPR<br>
+                                <input name="java" type="checkbox" required/> Souhlasím se zpracováním GDPR<br>
                         </div>
 
 
