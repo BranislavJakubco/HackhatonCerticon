d6136a8d19da02567c10e1d10444ca875176f69e
AsakaUchiha <kayoousek@gmail.com>
2022-11-04 18:25:27 +0100

    .

--- inc/vypis.inc.php
+++ inc/vypis.inc.php
@@ -1,17 +1,21 @@
 <?php
 
-$sp = mysqli_connect("localhost", "root", "", "udaje");
-mysqli_query($sp, "SET NAMES 'utf8';");
-$data = mysqli_query($sp, $dotaz);
-$dotaz = "SELECT * FROM `udaje`"; 
 while ($zaznam = mysqli_fetch_assoc($data)) {
-      echo '<div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">';
-      echo '<div class="card-header">{$zaznam["id_studenta"]}</div>';
-      echo '<div class="card-body">';
-      echo '<h5 class="card-title">{$zaznam["jmeno"]}</h5>';
-      echo '<p class="card-text">{$zaznam["rok_ukonceni"]}</p>';
-      echo '<p class="card-text">{$zaznam["programovaci_jazky"]}</p>';
-      echo '<p class="card-text">{$zaznam["preferovane_technologie"]}</p>';
+      echo '<div class="panel panel-default">';
+      echo '<div class="panel-heading">';
+      echo "ID: {$zaznam["id_studenta"]}";
+      echo '</div>';
+      echo '<p class="panel-body">';
+      echo "Jméno: {$zaznam["jmeno"]}";
+      echo '<p class="panel-body">';
+      echo "Rok Ukončení: {$zaznam["rok_ukonceni"]}";
+      echo '</p>';
+      echo '<p class="panel-body">';
+      echo "Jazyky: {$zaznam["programovaci_jazyky"]}";
+      echo '</p>';
+      echo '<p class="panel-body">';
+      echo "Technologie: {$zaznam["preferovane_technologie"]}";
+      echo '</p>';
       echo '</div></div>';
 
 
