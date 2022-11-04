53455b07a43038ccf3c017aa8cfb7ae468aebe0b
AsakaUchiha <kayoousek@gmail.com>
2022-11-04 17:19:31 +0100

    .

--- inc/formular.inc.php
+++ inc/formular.inc.php
@@ -4,6 +4,13 @@
                         <h1 style="margin-bottom: 6%; text-align: center; color: white">Kontaktujte nás!</h1>
                         <input name="name" type="text" class="kontakt-input" placeholder="Jméno" /> <br>
                         <input name="email" type="text" class="kontakt-input" placeholder="Email" /> <br>
+                        <input name="school" type="text" class="kontakt-input" placeholder="Škola" /> <br>
+                        <input name="yearEND" type="text" class="kontakt-input" placeholder="Rok ukončení" /> <br>
+                        <input name="prefTechnology" type="text" class="kontakt-input" placeholder="Preferované technologie" /> <br>
+                        <input name="GDPR" type="checkbox" class="kontakt-input" placeholder="Souhlasím se spracováním GDPR" /><br>
+
+
+
                         <input type="submit" value="Odeslat" />
                 </form>
         </div>
