52d51bda3c6b4fbf2447776044b3f32363b651ea
AsakaUchiha <kayoousek@gmail.com>
2022-11-04 14:48:04 +0100

    .

--- inc/rozcestnik.inc.php
+++ inc/rozcestnik.inc.php
@@ -1,15 +1,29 @@
-<!DOCTYPE html>
-<html lang="cs">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="assets/CSS.css">
-    <title>MakejvIT</title>
-</head>
-<body>
-    
-</body>
-</html>
+<div class="row">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header" style="margin-left: 10%;">
+                <a class="navbar-brand" href="#">MakejvIT</a>
+            </div>
+            <ul class="nav navbar-nav" style="float: right; margin-right: 10%;">
+                <li class="active"><a href="index.php?stranka=main">Domů</a></li>
+                <li><a href="index.php?stranka=formular">Formulář</a></li>
+                <li><a href="index.php?stranka=login">Login</a></li>
+                <li>
+                    <div class="dropdown">
+                        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="margin-top: 7%;">Databáze</button>
+
+                        <ul class="dropdown-menu">
+                            <li><a href="index.php?stranka=vypis">Zobrazení</a></li>
+                            <li><a href="index.php?stranka=editdatabase">Edit</a></li>
+                            <li><a href="index.php?stranka=import">Import</a></li>
+                        </ul>
+                    </div>
+                </li>
+    </nav>
+</div>
+
+
+</div>
+</div>
+
+</ul>
