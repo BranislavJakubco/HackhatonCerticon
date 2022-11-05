d7d68f1077245dbbb83b6244f19d5f5ee6e8c767
hemerkadavid@seznam.cz <hemerkadavid@seznam.cz>
2022-11-05 16:58:44 +0100

    .

--- inc/vypis.inc.php
+++ inc/vypis.inc.php
@@ -1,4 +1,4 @@
-
+<div class="container">
 <?php
 
 $sp = mysqli_connect("localhost","root","","udaje");
@@ -9,29 +9,48 @@ $dotaz="SELECT udaje.id_studenta, udaje.jmeno, udaje.email, udaje.rok_ukonceni, 
 $data= mysqli_query($sp,$dotaz);
 echo mysqli_error($sp);
 mysqli_close($sp);
+?>
+<form action="index.php?stranka=vypis" method="$_POST">
+<input type="text" name="rok" value="" ><p style="color: white;">Zadej rok</p><br>
+  <select>
+    <option value="Java"><p>Java</p></option>
+    <option value="Python"><p>Python</p></option>
+    <option value="C#"><p>C#</p></option>
+    <option value="C++"><p>C++</p></option>
+  </select>
+  <input type="text" name="other" value=""><p style="color: white;">Jazyky:</p><br>
+  <input type="submit" value="Odeslat">
+</form>
+</form>
+<?php
+// $rok=$_POST['rok'];
+// $vyber_roku="SELECT * from udaje where rok = '$rok'";
+// $ulozeni_roku=mysqli_query($sp,$vyber_roku);
+// echo $rok;
+
+
 
 while ($zaznam = mysqli_fetch_assoc($data)) {
-      echo '<div class="container">';
-      echo '<div class="row">';
-      echo '<div class=" col-sm-4">';
-      echo '<div class="panel panel-default";>';
-      echo '<div class="panel-heading">';
-      echo "ID: {$zaznam["id_studenta"]}";
-      echo '</div>';
-      echo '<p class="panel-body">';
-      echo "Jméno: {$zaznam["jmeno"]}";
-      echo '<p class="panel-body">';
-      echo "Rok Ukončení SŠ: {$zaznam["rok_ukonceni"]}";
-      echo '</p>';
-      echo '<p class="panel-body">';
-      echo "Jazyky: {$zaznam["jazyk"]}";
-      echo '</p>';
-      echo '<p class="panel-body">';
-      echo "Technologie: {$zaznam["preferovane_technologie"]}";
-      echo '</p>';
-      echo '</div></div>';
-
-
-      }
+  
+    echo '<div class="container">';
+    echo '<div class="row">';
+    echo '<div class=" col-sm-4">';
+    echo '<div class="panel panel-default";>';
+    echo '<div class="panel-heading">';
+    echo "ID: {$zaznam["id_studenta"]}";
+    echo '</div>';
+    echo '<p class="panel-body">';
+    echo "Jméno: {$zaznam["jmeno"]}";
+    echo '<p class="panel-body">';
+    echo "Rok Ukončení SŠ: {$zaznam["rok_ukonceni"]}";
+    echo '</p>';
+    echo '<p class="panel-body">';
+    echo "Jazyky: {$zaznam["jazyk"]}";
+    echo '</p>';
+    echo '<p class="panel-body">';
+    echo "Technologie: {$zaznam["preferovane_technologie"]}";
+    echo '</p>';
+    echo '</div></div>';    }
       ?>
+    </div>
     </div>
