3419bce627f41bfd7e8141742a19c6e7d6cda9b5
hemerkadavid@seznam.cz <hemerkadavid@seznam.cz>
2022-11-05 04:39:36 +0100

    .

--- PHP/insert.inc.php
+++ PHP/insert.inc.php
@@ -1,39 +1,207 @@
 <!DOCTYPE html>
 <html>
- 
+
 <head>
     <title>Insert Page page</title>
 </head>
- 
+
 <body>
-    
-<?php
+
+    <?php
+
 if (isset($_POST['GDPR'])) {
 
 $conn = mysqli_connect("localhost", "root", "", "udaje");
+mysqli_query($conn, "SET NAMES'utf8';");
+$id = $_REQUEST['id'];
 $jmeno = $_REQUEST['name'];
+$email = $_REQUEST['email'];
 $lokalita = $_REQUEST['lokalita'];
 $skola = $_REQUEST['school'];
 $rok_ukonceni = $_REQUEST['yearEND'];
-$programovaci_jazyky = $_REQUEST['jazyk'];
+$jazyk = $_REQUEST['jazyk'];
 $preferovane_technologie = $_REQUEST['prefTechnology'];
-if (isset($_POST['java'])) {
 
-    $java = $_REQUEST['jazyk'];
+$dotaz4 = "INSERT INTO jazyky (jazyk) values('$jazyk')";
+    $data4=mysqli_query($conn,$dotaz4);
+    echo mysqli_error($conn);
+
+    $dotaz5 = "INSERT INTO udaje VALUES ('$id','$jmeno',
+    '$email',
+    '$lokalita','$skola','$rok_ukonceni','$preferovane_technologie')";
+   $data5=mysqli_query($conn,$dotaz5);
+    echo mysqli_error($conn);
+
+if (isset($_POST['Java'])) {
+    
+    $jazyk = $_POST['Java'];
+    $dotaz2="SELECT id_jazyka from jazyky where jazyk = 'Java'";
+    $sql = "SELECT id_studenta from udaje where email = '$email'";
+    echo $sql;
+    $jazyk_data=mysqli_query($conn,$dotaz2);
+    $student_data=mysqli_query($conn,$sql);
+    $jazyk_id=0;
+    $student_id=0;
+    if ($jazyk_data) {
+         $row = mysqli_fetch_row($jazyk_data) ;
+          $jazyk_id=$row[0];
+          #echo $row[0];
+     }
+     if ($student_data) {
+        $row = mysqli_fetch_row($student_data) ;
+          $student_id=$row[0];
+       echo $row[0];
+    } 
+    #echo $student_id;
+    
+    $dotaz="INSERT INTO udaje_o_jazycích values ('$jazyk_id','$student_id')";
+    
+   echo "<p>$dotaz</p>";
+
+   # $data=mysqli_query($conn,$dotaz2);
+    #$data2=mysqli_query($conn,$sql);
+    
+    #mysqli_begin_transaction($conn);
+  #  echo mysqli_error($conn);
+    #$data3=mysqli_query($conn,$dotaz);
+   # echo mysqli_error($conn);
+    #mysqli_commit($conn);
+  #  echo mysqli_error($conn);
+    if (mysqli_query($conn,$dotaz) === TRUE) {
+  
+        echo "<p>New record created successfully</p>";
+} else {
+  echo "<p>Error: " . $dotaz . "<br>" . $conn->error."</p>";
 }
-if (isset($_POST['python'])) {
+}
+if (isset($_POST['Python'])) {
 
-    $python = $_REQUEST['jazyk'];
-}if (isset($_POST['c#'])) {
+    $jazyk = $_REQUEST['Python'];
+    $dotaz2="SELECT id_jazyka from jazyky where jazyk = 'Python'";
+    $sql = "SELECT id_studenta from udaje where email = '$email'";
+    echo $sql;
+    $jazyk_data=mysqli_query($conn,$dotaz2);
+    $student_data=mysqli_query($conn,$sql);
+    $jazyk_id=0;
+    $student_id=0;
+    if ($jazyk_data) {
+         $row = mysqli_fetch_row($jazyk_data) ;
+          $jazyk_id=$row[0];
+          #echo $row[0];
+     }
+     if ($student_data) {
+        $row = mysqli_fetch_row($student_data) ;
+          $student_id=$row[0];
+       echo $row[0];
+    } 
+    #echo $student_id;
+    
+    $dotaz="INSERT INTO udaje_o_jazycích values ('$jazyk_id','$student_id')";
+    
+   echo "<p>$dotaz</p>";
 
-    $cSharp = $_REQUEST['jazyk'];
-}if (isset($_POST['c++'])) {
+   # $data=mysqli_query($conn,$dotaz2);
+    #$data2=mysqli_query($conn,$sql);
+    
+    #mysqli_begin_transaction($conn);
+  #  echo mysqli_error($conn);
+    #$data3=mysqli_query($conn,$dotaz);
+   # echo mysqli_error($conn);
+    #mysqli_commit($conn);
+  #  echo mysqli_error($conn);
+    if (mysqli_query($conn,$dotaz) === TRUE) {
+  
+        echo "<p>New record created successfully</p>";
+} else {
+  echo "<p>Error: " . $dotaz . "<br>" . $conn->error."</p>";
+}
+}if (isset($_POST['CSharp'])) {
 
-    $cPlus = $_REQUEST['jazyk'];
+    $jazyk = $_REQUEST['CSharp'];
+    $dotaz2="SELECT id_jazyka from jazyky where jazyk = 'CSharp'";
+    $sql = "SELECT id_studenta from udaje where email = '$email'";
+    echo $sql;
+    $jazyk_data=mysqli_query($conn,$dotaz2);
+    $student_data=mysqli_query($conn,$sql);
+    $jazyk_id=0;
+    $student_id=0;
+    if ($jazyk_data) {
+         $row = mysqli_fetch_row($jazyk_data) ;
+          $jazyk_id=$row[0];
+          #echo $row[0];
+     }
+     if ($student_data) {
+        $row = mysqli_fetch_row($student_data) ;
+          $student_id=$row[0];
+       echo $row[0];
+    } 
+    #echo $student_id;
+    
+    $dotaz="INSERT INTO udaje_o_jazycích values ('$jazyk_id','$student_id')";
+    
+   echo "<p>$dotaz</p>";
+
+   # $data=mysqli_query($conn,$dotaz2);
+    #$data2=mysqli_query($conn,$sql);
+    
+    #mysqli_begin_transaction($conn);
+  #  echo mysqli_error($conn);
+    #$data3=mysqli_query($conn,$dotaz);
+   # echo mysqli_error($conn);
+    #mysqli_commit($conn);
+  #  echo mysqli_error($conn);
+    if (mysqli_query($conn,$dotaz) === TRUE) {
+  
+        echo "<p>New record created successfully</p>";
+} else {
+  echo "<p>Error: " . $dotaz . "<br>" . $conn->error."</p>";
+}
+}
+if (isset($_POST['C++'])) {
+
+    $jazyk = $_REQUEST['C++'];
+    $dotaz2="SELECT id_jazyka from jazyky where jazyk = 'C++'";
+    $sql = "SELECT id_studenta from udaje where email = '$email'";
+    echo $sql;
+    $jazyk_data=mysqli_query($conn,$dotaz2);
+    $student_data=mysqli_query($conn,$sql);
+    $jazyk_id=0;
+    $student_id=0;
+    if ($jazyk_data) {
+         $row = mysqli_fetch_row($jazyk_data) ;
+          $jazyk_id=$row[0];
+          #echo $row[0];
+     }
+     if ($student_data) {
+        $row = mysqli_fetch_row($student_data) ;
+          $student_id=$row[0];
+       echo $row[0];
+    } 
+    #echo $student_id;
+    
+    $dotaz="INSERT INTO udaje_o_jazycích values ('$jazyk_id','$student_id')";
+    
+   echo "<p>$dotaz</p>";
+
+   # $data=mysqli_query($conn,$dotaz2);
+    #$data2=mysqli_query($conn,$sql);
+    
+    #mysqli_begin_transaction($conn);
+  #  echo mysqli_error($conn);
+    #$data3=mysqli_query($conn,$dotaz);
+   # echo mysqli_error($conn);
+    #mysqli_commit($conn);
+  #  echo mysqli_error($conn);
+    if (mysqli_query($conn,$dotaz) === TRUE) {
+  
+        echo "<p>New record created successfully</p>";
+} else {
+  echo "<p>Error: " . $dotaz . "<br>" . $conn->error."</p>";
+}
 }
 
-$sql = "INSERT INTO udaje  VALUES ('$jmeno',
-'$lokalita','$skola','$rok_ukonceni','$preferovane_technologie')";
+
+
 mysqli_close($conn);
 }
 
@@ -44,6 +212,3 @@ mysqli_close($conn);
 </body>
 
 </html>
-
-
-
--- inc/formular.inc.php
+++ inc/formular.inc.php
@@ -2,14 +2,16 @@
         <div class="col-sm-12">
                 <form class="formstyle" action="./PHP/insert.inc.php" method="post">
                         <h1 style="margin-bottom: 6%; text-align: center; color: white">Kontaktujte nás!</h1>
+                        <input name="id" type="hidden" class="kontakt-input" placeholder="id" />
                         <input name="name" type="text" class="kontakt-input" placeholder="Jméno" />
+                        <input name="email" type="text" class="kontakt-input" placeholder="email" />
                         <input name="lokalita" type="text" class="kontakt-input" placeholder="Lokalita" /> <br>
                         <input name="school" type="text" class="kontakt-input" placeholder="Škola" /> <br>
                         <input name="yearEND" type="text" class="kontakt-input" placeholder="Rok ukončení" /> <br>
-                        <input name="java" type="checkbox" class="kontakt-input" />Java <br>
-                        <input name="python" type="checkbox" class="kontakt-input" />Python <br>
-                        <input name="cPlus" type="checkbox" class="kontakt-input" />C++ <br>
-                        <input name="cSharp" type="checkbox" class="kontakt-input" />C# <br>
+                        <input name="Java" type="checkbox" class="kontakt-input" />Java <br>
+                        <input name="Python" type="checkbox" class="kontakt-input" />Python <br>
+                        <input name="CSharp" type="checkbox" class="kontakt-input" />C# <br>
+                        <input name="C++" type="checkbox" class="kontakt-input" />C++ <br>
                         <input name="jazyk" type="text" class="kontakt-input" placeholder="Jiné programovací jazyky" /> <br>
                         <input name="prefTechnology" type="text" class="kontakt-input" placeholder="Preferované technologie" /> <br>
                         <input name="GDPR" type="checkbox" class="kontakt-input">Souhlasím se spracováním GDPR<br>
