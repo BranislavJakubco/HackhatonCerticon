681a00716dc09c8754ba26a1a7ceba8c3086926f
marmot753 <mart.motycka@atlas.cz>
2022-11-05 14:11:19 +0100

    .

--- inc/formular.inc.php
+++ inc/formular.inc.php
@@ -15,25 +15,24 @@ body {
         <div class="col-sm-12">
                 <form class="formstyle mid" action="./PHP/insert.inc.php" method="post">
                         <h1 style="margin-bottom: 6%; text-align: center;" class="textik">Kontaktujte nás!</h1>
-                        <input name="name" type="text" class="kontakt-input" placeholder="Jméno *" required />
-                        <input name="email" type="text" class="kontakt-input" placeholder="Email *" required />
-                        <input name="lokalita" type="text" class="kontakt-input" placeholder="Lokalita *" required /> <br>
-                        <input name="school" type="text" class="kontakt-input" placeholder="Škola *" required /> <br>
-                        <input name="yearEND" type="text" class="kontakt-input" placeholder="Rok ukončení *" required /> <br>
+                        <input name="name" type="text" class="kontakt-input" placeholder="Jméno *" required style="text-align: center;"/>
+                        <input name="email" type="text" class="kontakt-input" placeholder="Email *" required style="text-align: center;"/>
+                        <input name="lokalita" type="text" class="kontakt-input" placeholder="Lokalita *" required style="text-align: center;"/> <br>
+                        <input name="school" type="text" class="kontakt-input" placeholder="Škola *" required  style="text-align: center;"/> <br>
+                        <input name="yearEND" type="text" class="kontakt-input" placeholder="Rok ukončení *" required style="text-align: center;"/> <br>
 
-
+                        
                         <div class="kontakt-input">
                                 <p class="textik">Jaké programovací jazyky ovládáš?</p>
                                 <input name="Java" type="checkbox" class="textik" /> Java<br>
                                 <input name="Python" type="checkbox" class="textik" /> Python<br>
                                 <input name="CSharp" type="checkbox" class="textik" /> C#<br>
                                 <input name="CPlus" type="checkbox" class="textik" /> C++<br>
-
                         </div>
 
 
-                        <input name="jazyk" type="text" class="kontakt-input" placeholder="Jiné programovací jazyky" /> <br>
-                        <input name="prefTechnology" type="text" class="kontakt-input" placeholder="Preferované technologie *" required /> <br>
+                        <input name="jazyk" type="text" class="kontakt-input" placeholder="Jiné programovací jazyky" style="text-align: center;"/> <br>
+                        <input name="prefTechnology" type="text" class="kontakt-input" placeholder="Preferované technologie *" required style="text-align: center;"/> <br>
 
                         <div class="kontakt-input">
                                 <input name="GDPR" type="checkbox" required/> Souhlasím se zpracováním GDPR<br>
